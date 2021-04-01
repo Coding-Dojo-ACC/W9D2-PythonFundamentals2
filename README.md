34cec42b368a7f654e8b1e9cd237c4253fa1d7e2
dojo24 <mlongenberger@codingdojo.com>
2021-04-01 19:43:22 -0400

    saving

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # W9D2-PythonFundamentals2
+
+Python Tutor:  A good way to visualize your code at work:
+http://www.pythontutor.com/visualize.html#mode=edit
+
--- intro.py
+++ intro.py
@@ -32,8 +32,43 @@
 #     print(i+1, list[i])
 
 y = 24
-while y < 24:
-    print(y)
-    y = y -1
-else:
-    print("Printing")
+# while y < 24:
+#     print(y)
+#     y = y -1
+# else:
+#     print("Printing")
+
+
+# Functions
+
+
+# JS Version
+# function add(a, b) {
+#     return a + b
+#     console.log(a + b)
+# }
+# add(2, 4)
+
+def add(a,b):
+    x = a + b
+    return x
+    print(x)
+
+# add(2,4)
+
+# 0-500000 odd numbers add together them and print final sum
+# sum = 0
+# for x in range(1, 500000, 2):
+#     sum = sum + x
+
+# print(sum)
+# This above is done during learning or test environments as it is always active or will always run
+
+
+# This is how you would call it in real world so that it is only activated or run when called on like a submit button
+def finalSum():
+    sum = 0
+    for i in range(1, 500000, 2):
+        sum += i
+    print(sum)
+# finalSum()
