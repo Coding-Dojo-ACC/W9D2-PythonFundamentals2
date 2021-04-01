94cf762d88b368818d7ec98a5d98660916181d84
dojo24 <mlongenberger@codingdojo.com>
2021-04-01 18:19:47 -0400

    pushing to git

---
+++ intro.py
@@ -0,0 +1,39 @@
+# age = 42
+# if age > 40:
+#     print("Your over the hill")
+# else:
+#     print("Your still a youngun")
+
+
+# x = 25
+# if x > 40:
+#     print("Your over the hill")
+# elif x == 20:
+#     print("Your still a baby")
+# else:
+#     print(x)
+
+# for num in range(1, 5, 1):
+#     print(num)
+
+# for num in range(0, 25, 5):
+#     print(num)
+
+# for num in range(25, 0, -5):
+#     print(num)
+
+# for(num = 0; num > 25; num++)
+# (let num = 25; num>0; num--)
+
+
+# list = ["Hello", "Houston", "Happy", "Humpday"]
+# for i in range(0, len(list)): # for loop
+#     print(list[i])
+#     print(i+1, list[i])
+
+y = 24
+while y < 24:
+    print(y)
+    y = y -1
+else:
+    print("Printing")
